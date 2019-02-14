6dcd534f74bfbfcaa806b60e852e75ae35b123b5
Dariusz Kaszuba <dariusz.kaszuba11@gmail.com>
2019-02-14 11:28:37 +0100

    JP1

---
+++ src/jp1/Jp1.java
@@ -0,0 +1,11 @@
+package jp1;
+
+public class Jp1 {
+    public static void main(String[] args) {
+        String imie1="Brayan";
+        String imie2="Jessica";
+        String imie3="Janusz";
+
+        System.out.println(imie1+";"+imie2+";"+imie3);
+    }
+}
--- src/start/Hello.java
+++ src/start/Hello.java
@@ -1,9 +1,38 @@
 package start;
 
+import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
 // psvm public static void main
 //ctrl + shift + f10 -run
+//Ctrl +shoft + / komentarz calego
+//ctr;+D duplikowanie
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+        System.out.println("JRE Version: "+System.getProperty("java.runtime.version"));
+//        jaki JDK
+
+        int lunchTime = 123456;
+        int endTimer = -444444;
+        System.out.println(lunchTime);
+        System.out.println(endTimer);
+
+        double salary = 2100.99;
+        System.out.println(salary);
+        char signA = 'A';
+        char sign9 = '9';
+        char signTab = '\'';
+        char spacja = ' ';
+        System.out.println(signA);
+        System.out.println(sign9);
+        System.out.println(signTab);
+        System.out.println(spacja);
+//        System.out.println("signTab = " + signTab);
+
+        boolean flag=true;
+        boolean isLunch=false;
+        System.out.println(true);
+
+        System.out.println("isLunchrowna = " + isLunch);
+;    }
 }
