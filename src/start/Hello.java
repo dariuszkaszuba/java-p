5bfffac6d9d52230f2a5938c3fd970ae9a8b6298
Dariusz Kaszuba <dariusz.kaszuba11@gmail.com>
2019-02-14 10:05:03 +0100

    START

---
+++ src/start/Hello.java
@@ -0,0 +1,9 @@
+package start;
+
+// psvm public static void main
+//ctrl + shift + f10 -run
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
